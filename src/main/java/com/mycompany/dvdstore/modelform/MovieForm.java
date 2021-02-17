414c0bcc935b9d58cac3ef8035c46601d4081ab9
Thierry WADJI <wadjith@yahoo.fr>
2021-02-17 15:35:46 +0100

    Afficher les messages d'erreurs

--- src/main/java/com/mycompany/dvdstore/modelform/MovieForm.java
+++ src/main/java/com/mycompany/dvdstore/modelform/MovieForm.java
@@ -4,12 +4,12 @@ import javax.validation.constraints.*;
 
 public class MovieForm {
 
-    @NotBlank
-    @Size(max = 20)
+    @NotBlank(message = "Veuillez entrer un titre")
+    @Size(max = 20, message = "Entrez un titre de 20 caractères au plus")
     private String title;
-    @NotBlank
+    @NotBlank(message = "Veuillez choisir un genre")
     private String genre;
-    @Size(max = 255)
+    @Size(max = 255, message = "Entrez une description de 255 caractères au plus")
     private String description;
 
     public String getTitle() {
